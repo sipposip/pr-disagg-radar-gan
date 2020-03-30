bdf40fd8379e675851a9a1f46b6269e0d6126d45
Sebastian <sebastian.scher@misu.su.se>
2020-03-30 13:30:40 +0200

    updated plots to versions in paper, updated readme

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from raindisagg_gan_pretrained import generate_scenarios, plot_scenarios
 
 ndomain = 16 #the domain used in the traing of the GAN. must be the same here
 # create made-up input conditions (including empty last channel dimension) with 10mm/day at every gridpoint
-# in a real application, here you would use your own data (but you still have to normalize it with norm_scale)
+# in a real application, here you would use your own data  (in mm/h).
 cond1 = 10 * np.ones((ndomain, ndomain, 1))
 # generate subdaily scenarios
 n_scenarios = 10
