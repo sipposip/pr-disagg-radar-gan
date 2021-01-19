ea19a6d900103a32ae8458799bdc959d7716cb43
Sebastian <sebastian.scher@misu.su.se>
2021-01-19 11:18:46 +0100

    added experiment with large domain added analysis script for log spectral distance (not yet finished)

---
+++ alternative_domains/compute_valid_indices_largedomain.py
@@ -0,0 +1,100 @@
+#! /pfs/nobackup/home/s/sebsc/miniconda3/envs/pr-disagg-env/bin/python
+"""
+
+read in formatted data (outut from reformat_data.py),
+and determined all valid training samples from it.
+
+valid training samples are all ndomain x ndomain boxes that are free of NaN values,
+and where the daily sum exceeds a certain threshold at at least a certain amount of points
+(default 5mm on 20 points on a 16x16 domain)
+
+the "sweep" over the domain is controlled by the "stride" parameter. If it is 1, then all possible boxes
+are tried out (including those with overlap). with strid=ndomain, all non-overlapping boxes are scanned.
+
+output: .pkl file containing the indices of the training samples
+
+these indices can then be used the following way:
+idcs = final_valid_idcs[0]
+sub = data[idcs[0],:,idcs[1]:idcs[1]+ndomain,idcs[2]:idcs[2]+ndomain]
+
+@internal: run on misu160 and kebnekaise
+
+@author: Sebastian Scher
+"""
+
+import os
+import pickle
+import numpy as np
+import numba
+from dask.diagnostics import ProgressBar
+
+pbar = ProgressBar()
+pbar.register()
+
+os.system('mkdir -p data')
+# the data is not complete (not all days are available)
+# PARAMS
+# for training data:
+# startdate = '20090101'
+# enddate = '20161231'
+# for test data:
+startdate = '20170101'
+enddate = '20181231'
+ndomain = 64  # gridpoints
+stride = 16  # |ndomain # in which steps to scan the whole domain
+tres = 1
+tp_thresh_daily = 5  # mm. in the radardate the unit is mm/h, but then on 5 minutes steps.
+# the conversion is done automatically in this script
+n_thresh = 40
+# END PARAMS
+
+if ndomain % 2 != 0:
+    raise ValueError(f'ndomain must be an even number')
+
+datapath = '/home/s/sebsc/pfs/pr_disagg/smhi_radar/preprocessed'
+
+ifile = f'{datapath}/{startdate}-{enddate}_tres{tres}.npy'
+
+data = np.load(ifile, mmap_mode='r')
+
+if len(data.shape) != 4:
+    raise ValueError(f'data has wrong number of dimensions {len(data.shape)} instead of 4')
+
+# compute daily sum, which is the sum over the hour axis
+n_days,nhour, ny, nx = data.shape
+
+
+# compute all valid indices
+# for this, we try out all ndomain x ndomain squares shifted by strides, and check whether they have any missing data,
+# and if not, whether they adhere to the criteria set by tp_thresh_daily and n_thresh
+# since this contains many for loops, we speed it up with numba
+
+
+@numba.jit
+def filter(data):
+    final_valid_idcs = []
+    # loop over timeslices
+    for tidx in numba.prange(n_days):
+        print(tidx, '/', n_days)
+        # daily sum
+        sub = np.sum(data[tidx],axis=0)
+        # loop over all possible boxes
+        for ii in range(0, ny - ndomain, stride):
+            for jj in range(0, nx - ndomain, stride):
+                subsub = sub[ii:ii + ndomain, jj:jj + ndomain]
+                # check for nan values
+                if not np.any(np.isnan(subsub)):
+                    # if at least n_thresh points are above the threshold,
+                    # we use this box
+                    if np.sum(subsub > tp_thresh_daily) >= n_thresh:
+                        final_valid_idcs.append((tidx, ii, jj))
+
+    return final_valid_idcs
+
+
+final_valid_idcs = filter(data)
+
+params = f'{startdate}-{enddate}-tp_thresh_daily{tp_thresh_daily}_n_thresh{n_thresh}_ndomain{ndomain}_stride{stride}'
+pickle.dump(final_valid_idcs, open(f'data/valid_indices_smhi_radar_{params}.pkl', 'wb'))
+
+print(f'found {len(final_valid_idcs)} valid samples')
---
+++ log_spectral_distance.py
@@ -0,0 +1,146 @@
+"""
+
+adapted from https://www.astrobetter.com/blog/2010/03/03/fourier-transforms-of-images-in-python/
+
+"""
+
+import pickle
+import numpy as np
+from scipy import fftpack
+from tqdm import tqdm, trange
+import numba
+import seaborn as sns
+
+from pylab import plt
+
+def azimuthal_verage(image, center=None):
+    """
+    Calculate the azimuthally averaged radial profile.
+
+    image - The 2D image
+    center - The [x,y] pixel coordinates used as the center. The default is
+             None, which then uses the center of the image (including
+             fracitonal pixels).
+
+    """
+    # Calculate the indices from the image
+    y, x = np.indices(image.shape)
+
+    if not center:
+        center = np.array([(x.max() - x.min()) / 2.0, (x.max() - x.min()) / 2.0])
+
+    r = np.hypot(x - center[0], y - center[1])
+
+    # Get sorted radii
+    ind = np.argsort(r.flat)
+    r_sorted = r.flat[ind]
+    i_sorted = image.flat[ind]
+
+    # Get the integer part of the radii (bin size = 1)
+    r_int = r_sorted.astype(int)
+
+    # Find all pixels that fall within each radial bin.
+    deltar = r_int[1:] - r_int[:-1]  # Assumes all radii represented
+    rind = np.where(deltar)[0]  # location of changed radius
+    nr = rind[1:] - rind[:-1]  # number of radius bin
+
+    # Cumulative sum to figure out sums for each radius bin
+    csim = np.cumsum(i_sorted, dtype=float)
+    tbin = csim[rind[1:]] - csim[rind[:-1]]
+
+    radial_prof = tbin / nr
+
+    return radial_prof
+
+
+def compute_radial_spectrum(x):
+    # 2d ffft
+    f1 = fftpack.fft2(x)
+    f2 = fftpack.fftshift(f1)
+    psd2D = np.abs(f2) ** 2
+    psd1D = azimuthal_verage(psd2D)
+    return psd1D
+
+
+@numba.njit(parallel=True)
+def log_spectral_distance(ps1, ps2):
+    """compute the log spectral distance between
+    two power spectra
+    the integral over the frequency is approximated via a sum.
+    """
+    n_omega = len(ps1)
+    assert (len(ps2) == n_omega)
+    return np.sqrt(np.sum((10 * np.log10(ps1 / ps2)) ** 2)) * 1 / n_omega
+
+
+generated = np.load('/climstorage/sebastian/pr_disagg/data/generated_samples.npy')
+real = np.load('/climstorage/sebastian/pr_disagg/data/real_samples.npy')
+
+# hack
+n_samples = 500
+generated = generated[:n_samples]
+real = real[:n_samples]
+
+
+# flatten the hour dimension into the sample dimension
+
+generated_flathour = generated.reshape(-1,*generated.shape[2:])
+real_flathour = real.reshape(-1,*real.shape[2:])
+
+
+
+spectra_real = np.array([compute_radial_spectrum(x) for x in tqdm(real_flathour)])
+#
+spectra_gen = np.array([compute_radial_spectrum(x) for x in tqdm(generated_flathour)])
+#
+# np.save('/climstorage/sebastian/pr_disagg/data/sprectra_real.npy', spectra_real)
+# np.save('/climstorage/sebastian/pr_disagg/data/sprectra_gen.npy', spectra_gen)
+# spectra_real = np.load('/climstorage/sebastian/pr_disagg/data/sprectra_real.npy')
+# spectra_gen = np.load('/climstorage/sebastian/pr_disagg/data/sprectra_gen.npy')
+
+
+n = len(spectra_real)
+
+@numba.njit(parallel=True)
+def compute_dists(x,y):
+
+    n=len(x)
+    dists = np.zeros(n**2) # this needs too much memory!!!
+    for i in range(n):
+        print(i,n)
+        for j in range(n):
+            if i != j:
+                d = log_spectral_distance(x[i], y[j])
+                dists[i*n+j] = d
+
+    return dists
+
+
+dists_real = compute_dists(spectra_real, spectra_real)
+dists_gen = compute_dists(spectra_gen, spectra_gen)
+dists_between_gen_and_real = compute_dists(spectra_gen, spectra_real)
+# remove diagonal elements (they are zero)
+indices = np.arange(n ** 2)
+diags = np.arange(0, n ** 2, n)
+indices_no_diag = [e for e in indices if e not in diags]
+
+dists_real = dists_real[indices_no_diag]
+dists_gen = dists_gen[indices_no_diag]
+dists_between_gen_and_real = dists_between_gen_and_real[indices_no_diag]
+
+res = {'real':dists_real,'gen':dists_real,
+       'between':dists_between_gen_and_real}
+pickle.dump(res,open('log_spectral_distances.pkl','wb'))
+
+#todo: in dists_real, there are a lot of inf (which areise due to division by zero in the log
+# spectral distances.
+# how should we deal with them??
+
+plt.figure()
+sns.kdeplot(dists_real, label='obs')
+sns.kdeplot(dists_gen, label='generated')
+sns.kdeplot(dists_between_gen_and_real, label='between obs and generated')
+plt.legend()
+sns.despine()
+plt.savefig(f'plots/log_spectral_distances_n{n_samples}.svg')
+
