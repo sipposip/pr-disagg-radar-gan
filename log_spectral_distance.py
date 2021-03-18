730f9203a54fb1f552b2f68417d5b86bed2ee412
Sebastian <sebastian.scher@misu.su.se>
2021-03-18 10:00:02 +0100

    changed to higher sample number and changed saving format (numpy instead of pickle)

--- log_spectral_distance.py
+++ log_spectral_distance.py
@@ -5,12 +5,15 @@ adapted from https://www.astrobetter.com/blog/2010/03/03/fourier-transforms-of-i
 """
 
 import pickle
+import matplotlib
+matplotlib.use('agg')
 import numpy as np
 from scipy import fftpack
 from tqdm import tqdm, trange
 import numba
 import seaborn as sns
 from pylab import plt
+
 
 
 def azimuthal_average(image, center=None):
@@ -73,42 +76,41 @@ def log_spectral_distance(ps1, ps2):
     return np.sqrt(np.sum((10 * np.log10(ps1 / ps2)) ** 2)) * 1 / n_omega
 
 
-generated = np.load('/climstorage/sebastian/pr_disagg/data/generated_samples.npy')
-real = np.load('/climstorage/sebastian/pr_disagg/data/real_samples.npy')
-generated_rainfarm = np.load('/climstorage/sebastian/pr_disagg/data/generated_samples_rainfarm.npy')
-
+# generated = np.load('/climstorage/sebastian/pr_disagg/data/generated_samples.npy')
+# real = np.load('/climstorage/sebastian/pr_disagg/data/real_samples.npy')
+# generated_rainfarm = np.load('/climstorage/sebastian/pr_disagg/data/generated_samples_rainfarm.npy')
+generated = np.load('data/generated_samples.npy')
+real = np.load('data/real_samples.npy')
+generated_rainfarm = np.load('data/generated_samples_rainfarm.npy')
 
 n_samples = 1000
 generated = generated[:n_samples]
 real = real[:n_samples]
 generated_rainfarm = generated_rainfarm[:n_samples]
 
-
 # flatten the hour dimension into the sample dimension
 
-generated_rainfarm_flathour = generated_rainfarm.reshape(-1,*generated.shape[2:])
-generated_flathour = generated.reshape(-1,*generated.shape[2:])
-real_flathour = real.reshape(-1,*real.shape[2:])
-
+generated_rainfarm_flathour = generated_rainfarm.reshape(-1, *generated.shape[2:])
+generated_flathour = generated.reshape(-1, *generated.shape[2:])
+real_flathour = real.reshape(-1, *real.shape[2:])
 
 spectra_real = np.array([compute_radial_spectrum(x) for x in tqdm(real_flathour)])
 spectra_gen = np.array([compute_radial_spectrum(x) for x in tqdm(generated_flathour)])
 spectra_gen_rainfarm = np.array([compute_radial_spectrum(x) for x in tqdm(generated_rainfarm_flathour)])
 
-
 n = len(spectra_real)
 
-@numba.njit(parallel=True)
-def compute_dists(x,y):
 
-    n=len(x)
-    dists = np.zeros(n**2) # this needs too much memory!!!
+@numba.njit(parallel=True)
+def compute_dists(x, y):
+    n = len(x)
+    dists = np.zeros(n ** 2)  # this needs too much memory!!!
     for i in range(n):
-        print(i,n)
+        print(i, n)
         for j in range(n):
             if i != j:
                 d = log_spectral_distance(x[i], y[j])
-                dists[i*n+j] = d
+                dists[i * n + j] = d
 
     return dists
 
@@ -127,15 +129,11 @@ dists_real = dists_real[indices_no_diag]
 dists_gen = dists_gen[indices_no_diag]
 dists_between_gen_and_real = dists_between_gen_and_real[indices_no_diag]
 
-res = {'real':dists_real,'gen':dists_gen,
-       'between_gen_real':dists_between_gen_and_real,
-       'between_gen_rainfarm_real':dists_between_gen_rainfarm_and_real,
-       }
-pickle.dump(res,open('log_spectral_distances.pkl','wb'))
-
-#todo: in dists_real, there are a lot of inf (which areise due to division by zero in the log
-# spectral distances.
-# how should we deal with them??
+np.save(f'log_spectral_distances_real_n{n_samples}.npy', dists_real)
+np.save(f'log_spectral_distances_gen_n{n_samples}.npy', dists_gen)
+np.save(f'log_spectral_distances_gen_rainfarm_n{n_samples}.npy', dists_gen_rainfarm)
+np.save(f'log_spectral_distances_between_gen_real_n{n_samples}.npy', dists_between_gen_and_real)
+np.save(f'log_spectral_distances_between_gen_rainfarm_real_n{n_samples}.npy', dists_between_gen_rainfarm_and_real)
 
 sns.set_palette('colorblind')
 
@@ -149,4 +147,3 @@ plt.xlabel('log spectral distance')
 plt.legend()
 sns.despine()
 plt.savefig(f'plots/log_spectral_distances_n{n_samples}.svg')
-
